596bebbfe8b4fa1db96a2431b95c25221d7c05d6
yogeshkb-yoyo <yoyokb404@gmail.com>
2021-10-10 15:52:54 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import User from "./components/User";
+
+
+function App() {
+  return (
+    <div>
+    <h1>Hello</h1>
+    <User/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/User.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const User = () => {
+    return (
+        <div>
+            <h1>HII</h1>
+        </div>
+    )
+}
+
+export default User;
